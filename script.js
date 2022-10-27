fedd2ba9e2755b0db6e3c9ed9378b1383257ea20
kd1042 <73589096+kd1042@users.noreply.github.com>
2022-10-27 13:19:18 +0530

    Project Finished

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let uservalue = 0;
 let historyvalue = 0;
 let addition = false;
+let substrct = false;
+let multiply = false;
+let divide   = false;
 const userinput = document.querySelector('.userinput');
 const hist = document.querySelector('.history');
 
@@ -15,7 +18,7 @@ function getvalue(value){
   }
 }
 
-const add = document.getElementById('add');
+const add = document.getElementById('add'); // addition button
 add.addEventListener('click',function(){
     historyvalue = uservalue;
     uservalue = 0;
@@ -25,11 +28,63 @@ add.addEventListener('click',function(){
 
 })
 
+const sub = document.getElementById('sub'); // subtract button
+sub.addEventListener('click',function(){
+    historyvalue = uservalue;
+    uservalue = 0;
+    userinput.textContent = 0;
+    hist.textContent = `${historyvalue} -`;
+    substrct = true;
+})
+
+const multi = document.getElementById('mul'); //multiply button
+multi.addEventListener('click',function(){
+    historyvalue = uservalue;
+    uservalue = 0;
+    userinput.textContent = 0;
+    hist.textContent = `${historyvalue} *`;
+    multiply = true;
+})
+
+const divd = document.getElementById('divide'); //divide button
+divd.addEventListener('click',function(){
+    historyvalue = uservalue;
+    uservalue = 0;
+    userinput.textContent = 0;
+    hist.textContent = `${historyvalue} /`;
+    divide = true;
+})
+
+// equals button (if it matches with any of the 4 operators evalute the function)
 function evaluat(){
-    if(addition === true){
-       userinput.textContent = Number(historyvalue) + Number(uservalue);
+    if(addition === true){   
+       hist.textContent = `${historyvalue} + ${uservalue}`
+       ans = Number(historyvalue) + Number(uservalue)
+       uservalue = ans;
+       userinput.textContent = `${ans}` ;
        addition = false;
     }
+    else if(substrct === true){
+       hist.textContent = `${historyvalue} - ${uservalue}`
+       ans = Number(historyvalue) - Number(uservalue)
+       uservalue = ans;
+       userinput.textContent = `${ans}` ;
+       substrct = false;
+    }
+    else if(multiply === true){
+      hist.textContent = `${historyvalue} * ${uservalue}`
+      ans = Number(historyvalue) * Number(uservalue)
+      uservalue = ans;
+      userinput.textContent = `${ans}` ;
+      multiply = false;
+   }
+    else if(divide === true){
+      hist.textContent = `${historyvalue} / ${uservalue}`
+      ans = Number(historyvalue) / Number(uservalue)
+      uservalue = ans;
+      userinput.textContent = `${ans}` ;
+      divide = false;
+ }
 }
 
 const clear = document.getElementById('clear');
